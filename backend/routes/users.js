5ce3b5134c8b8004b7409db720faf917953abdc4
JCode918 <Willard.Joseph26@gmail.com>
2020-07-22 22:01:44 -0400

    Added Some Routes to test the Login Function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
-let User = require('../models/user.model')
+const connectEnsureLogin = require('connect-ensure-login');
+const passport = require('passport')
+
+let User = require('../models/user.model');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 router.route('/').get((req, res) => {
     User.find()
@@ -17,5 +21,24 @@ router.route('/add').post((req, res) => {
         .then(() => res.json('User added!'))
         .catch((err) => res.status(400).json('Error' + err))
 });
+
+router.route('/login').post((req, res, next) => {
+    passport.authenticate('local', (err, User, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!User) {
+            return res.redirect('/login?info' + info)
+        }
+        req.logIn(User, function (err) {
+            if (err) {
+                return next(err)
+            }
+            return res.redirect('/')
+        })
+    })(req, res, next)
+})
+
+router.route('/login').get()
 
 module.exports = router
